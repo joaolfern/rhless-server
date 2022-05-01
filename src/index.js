f64507c52cd43ab5c0df096310b4d5be71c41f50
joaoaoluiz <joaoaoluiz@protonmail.com>
2022-05-01 00:52:23 -0300

    Setup

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+
+dotenv.config()
+
+const app = express()
+
+app.use(cors())
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.listen(process.env.PORT, () => console.log('Listening on port ' + process.env.PORT))
