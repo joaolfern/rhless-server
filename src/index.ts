9d7f713dcfd5b8ed54f3e0ca846b2de9db73b92c
joaoaoluiz <joaoaoluiz@protonmail.com>
2022-05-01 16:58:13 -0300

    Fix index file extension

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import cors from 'cors'
+const express = require('express')
+const dotenv = require('dotenv')
+const cors = require('cors')
 
 dotenv.config()
 
